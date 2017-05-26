c2f4ac4bc8a44e91d2e28b0e786151fc64e7825f
innayan <inna_yan@mail.ru>
2017-05-26 19:05:21 +0300

    tttt

---
+++ CalculatorXTest/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using Calculator;
+
+namespace CalculatorXTest
+  
+{ [TestFixture]
+    public class CalculatorTest
+    { 
+    
+    [Test]
+
+        public void TestSumma()
+    {
+        Assert.AreEqual(CalculatorX.Summa(2, 3), 5);
+          
+   }
+    [Test]
+
+    public void TestSumma1()
+    {
+        Assert.AreEqual(CalculatorX.Summa(2, 2), 5);
+
+    }
+      
+       
+
+    }
+}
